d9bc15eafdc386d50117105c0ec0edc804e8bfb0
Chancelor Assiamah <chanceyassiamah@gmail.com>
2025-11-01 02:32:15 -0400

    API: added functionality that allows for a MCP server to authenticate itself and access protected resources.

--- src/core/config.py
+++ src/core/config.py
@@ -14,8 +14,14 @@ class Settings(BaseSettings):
     openai_api_key: SecretStr | None = None
     jwt_secret_key: SecretStr = SecretStr(token_hex(16))
     jwt_algorithm: SecretStr = SecretStr("HS256")
-    jwt_access_token_expire_minutes: int = 30
+    user_access_token_expire_minutes: int = 30
     db_url: str | URL | None = None
+    jwt_audience: SecretStr = SecretStr("licenseguard-api")
+    mcp_client_id: SecretStr | None = None
+    mcp_client_secret: SecretStr | None = None
+    mcp_access_token_expire_minutes: int = 10
+    # must be a string of space-delimited scopes
+    mcp_required_scopes: SecretStr = SecretStr("analyze:run")
 
 
 def get_settings() -> Settings:
--- src/srv/security.py
+++ src/srv/security.py
@@ -5,7 +5,6 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
-from pydantic import SecretStr
 from sqlmodel.ext.asyncio.session import AsyncSession
 from core.config import get_settings
 from db.session import get_session
@@ -14,6 +13,11 @@ from srv.schemas import TokenData, UserPublic
 
 # import the JWT config variables
 settings = get_settings()
+JWT_SECRET_KEY = settings.jwt_secret_key
+JWT_ALGORITHM = settings.jwt_algorithm
+JWT_AUDIENCE = settings.jwt_audience
+MCP_CLIENT_ID = settings.mcp_client_id
+MCP_REQUIRED_SCOPES = settings.mcp_required_scopes
 
 # setup password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -41,7 +45,7 @@ def create_access_token(
     # otherwise, use the default (30 minutes)
     else:
         expire = datetime.now(
-            timezone.utc) + timedelta(minutes=settings.jwt_access_token_expire_minutes)
+            timezone.utc) + timedelta(minutes=settings.user_access_token_expire_minutes)
     payload.update({"exp": expire})
     access_token = jwt.encode(
         payload,
@@ -75,14 +79,51 @@ async def get_current_user(
         if username is None:
             print("Couldn't find the username provided in the JWT!")
             raise credentials_exception
-        token_data = TokenData(username=username)
-    except InvalidTokenError:
-        print("Couldn't verify the JWT.")
-        raise credentials_exception
 
-    user = await users_service.get_user(session, username=token_data.username)
-    if user is None:
-        print(
-            f"Couldn't find a user with the username '{token_data.username}'.")
+        # the audience will be null if the JWT is intended for human users
+        if payload.get("aud") is None:
+            token_data = TokenData(username=username)
+            user = await users_service.get_user(session, username=token_data.username)
+            if user is None:
+                print(
+                    f"Couldn't find a user with the username '{token_data.username}'.")
+                raise credentials_exception
+            return user
+    except InvalidTokenError:
+        print("Couldn't verify the JWT is intended for a user. Attempting to verify for MCP server...")
+        pass
+
+    try:
+        payload = jwt.decode(
+            token,
+            JWT_SECRET_KEY.get_secret_value(),
+            algorithms=[JWT_ALGORITHM.get_secret_value()],
+            audience=JWT_AUDIENCE.get_secret_value()
+        )
+
+        # verify that the MCP client ID was provided
+        if MCP_CLIENT_ID is None:
+            raise RuntimeError(
+                "MCP_CLIENT_ID is required to retrieve a JWT for the MCP server.")
+
+        # verify that the payload includes the correct MCP client ID and required scopes
+        client_id: str = payload.get("sub")
+        if client_id != MCP_CLIENT_ID.get_secret_value():
+            print(
+                "Couldn't find the correct client_id for the MCP server provided in the JWT!")
+            raise credentials_exception
+        required_scopes = MCP_REQUIRED_SCOPES.get_secret_value().split(" ")
+        scopes = payload.get("scope")
+        for required in required_scopes:
+            assert required in scopes, f"The required scope '{required}' is missing from the payload's scopes ('{scopes}')."
+
+        # retrieve the "service" user associated with the MCP server
+        service_user = await users_service.get_user(session, username=client_id)
+        if service_user is None:
+            print(
+                f"Couldn't find a user with the username '{client_id}'.")
+            raise credentials_exception
+        return service_user
+    except InvalidTokenError:
+        print("Couldn't verify the JWT!")
         raise credentials_exception
-    return user
